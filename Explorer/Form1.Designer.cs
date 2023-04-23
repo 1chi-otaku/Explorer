391dda7885878d050609b783c5cbb9162fb56b04
Serhii <mywaifuqueen@gmail.com>
2023-04-23 15:15:11 +0100

    Add project files.

---
+++ Explorer/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.largeIconToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 24);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(800, 426);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.BeforeSelect += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeSelect);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(222, 28);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(577, 419);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listToolStripMenuItem,
+            this.tableToolStripMenuItem,
+            this.tileToolStripMenuItem,
+            this.largeIconToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // listToolStripMenuItem
+            // 
+            this.listToolStripMenuItem.Name = "listToolStripMenuItem";
+            this.listToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.listToolStripMenuItem.Text = "List";
+            this.listToolStripMenuItem.Click += new System.EventHandler(this.listToolStripMenuItem_Click);
+            // 
+            // tableToolStripMenuItem
+            // 
+            this.tableToolStripMenuItem.Name = "tableToolStripMenuItem";
+            this.tableToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tableToolStripMenuItem.Text = "Table";
+            this.tableToolStripMenuItem.Click += new System.EventHandler(this.tableToolStripMenuItem_Click);
+            // 
+            // tileToolStripMenuItem
+            // 
+            this.tileToolStripMenuItem.Name = "tileToolStripMenuItem";
+            this.tileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tileToolStripMenuItem.Text = "Tile";
+            this.tileToolStripMenuItem.Click += new System.EventHandler(this.tileToolStripMenuItem_Click);
+            // 
+            // largeIconToolStripMenuItem
+            // 
+            this.largeIconToolStripMenuItem.Name = "largeIconToolStripMenuItem";
+            this.largeIconToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.largeIconToolStripMenuItem.Text = "Large Icon";
+            this.largeIconToolStripMenuItem.Click += new System.EventHandler(this.largeIconToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem largeIconToolStripMenuItem;
+    }
+}
+
---
+++ Explorer/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Explorer
+{
+    public partial class Form1 : Form
+    {
+        ImageList image_list1 = new ImageList(); // список изображений для хранения малых значков
+        ImageList image_list2 = new ImageList(); // список изображений для хранения больших значков
+        string pathToFolder = @"C:/";
+
+        public Form1()
+        {
+            InitializeComponent();
+            image_list1.ColorDepth = ColorDepth.Depth32Bit;
+            image_list1.ImageSize = new Size(16, 16);
+            listView1.SmallImageList = image_list1;
+            image_list2.ColorDepth = ColorDepth.Depth32Bit;
+            image_list2.ImageSize = new Size(32, 32);
+            listView1.LargeImageList = image_list2;
+
+
+            string[] files = Directory.GetFiles(pathToFolder);
+            string[] directories = Directory.GetDirectories(pathToFolder);
+            Icon icon = new Icon(@"../../CLSDFOLD.ICO");
+            image_list1.Images.Add(icon);
+            image_list2.Images.Add(icon);
+
+            foreach (string dir in directories)
+            {
+                listView1.Items.Add(dir, 0);
+            }
+            int index = 1;
+            foreach (string file in files)
+            {
+                icon = Icon.ExtractAssociatedIcon(file);
+                image_list1.Images.Add(icon);
+                image_list2.Images.Add(icon);
+                listView1.Items.Add(file, index++);
+            }
+
+            string[] drives = System.IO.Directory.GetLogicalDrives();
+
+            foreach (string drive in drives)
+            {
+                TreeNode driveNode = new TreeNode(drive);
+                driveNode.Tag = drive;
+                driveNode.Nodes.Add(new TreeNode());
+
+                treeView1.Nodes.Add(driveNode);
+            }
+        }
+        private void listToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.SmallIcon;
+        }
+
+        private void tableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.List;
+        }
+
+        private void tileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.Tile;
+        }
+
+        private void largeIconToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.LargeIcon;
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode selectedNode = e.Node;
+
+            if (selectedNode.Nodes.Count == 1 && selectedNode.Nodes[0].Text == "")
+            {
+                selectedNode.Nodes.Clear();
+
+                string[] files = System.IO.Directory.GetFiles(selectedNode.Tag.ToString());
+                string[] directories = System.IO.Directory.GetDirectories(selectedNode.Tag.ToString());
+                int index = 1;
+                foreach (string file in files)
+                {
+                    Icon icon = Icon.ExtractAssociatedIcon(file);
+                    image_list1.Images.Add(icon);
+                    image_list2.Images.Add(icon);
+                    listView1.Items.Add(file, index++);
+                    TreeNode fileNode = new TreeNode(System.IO.Path.GetFileName(file));
+                    fileNode.Tag = file;
+                    selectedNode.Nodes.Add(fileNode);
+                }
+
+                foreach (string directory in directories)
+                {
+                    TreeNode directoryNode = new TreeNode(System.IO.Path.GetFileName(directory));
+                    directoryNode.Tag = directory;
+                    directoryNode.Nodes.Add(new TreeNode());
+                    selectedNode.Nodes.Add(directoryNode);
+                }
+            }
+        }
+
+        private void treeView1_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode expandingNode = e.Node;
+
+            if (expandingNode.Nodes.Count == 1 && expandingNode.Nodes[0].Text == "")
+            {
+                expandingNode.Nodes.Clear();
+                string[] files = System.IO.Directory.GetFiles(expandingNode.Tag.ToString());
+                string[] directories = System.IO.Directory.GetDirectories(expandingNode.Tag.ToString());
+                int index = 1;
+                foreach (string file in files)
+                {
+                    Icon icon = Icon.ExtractAssociatedIcon(file);
+                    image_list1.Images.Add(icon);
+                    image_list2.Images.Add(icon);
+                    listView1.Items.Add(file, index++);
+                    TreeNode fileNode = new TreeNode(System.IO.Path.GetFileName(file));
+                    fileNode.Tag = file;
+                    expandingNode.Nodes.Add(fileNode);
+                }
+
+                foreach (string directory in directories)
+                {
+                    TreeNode directoryNode = new TreeNode(System.IO.Path.GetFileName(directory));
+                    directoryNode.Tag = directory;
+                    directoryNode.Nodes.Add(new TreeNode());
+                    expandingNode.Nodes.Add(directoryNode);
+                }
+            }
+        }
+    }
+}
